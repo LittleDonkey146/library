02975f532a51114b8815e5d31abe2464c8c8f5be
littledonkey146 <alexandrosgka@gmail.com>
2022-06-14 00:52:13 +0300

    Started some work

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-function Books(title, author, pages, read) {
+const addBook = document.querySelector(".add-book");
+addBook.addEventListener('click', newBook);
+
+let library = [];
+
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -8,5 +13,17 @@ function Books(title, author, pages, read) {
     }
 }
 
-const hobbit = new Books("Hobbit", "Alex", "25", "not read");
-console.log(hobbit.info());
+function addBookToLibrary(newBook) {
+    library.push(newBook);
+}
+
+// const hobbit = new Book("Hobbit", "Alex", "25", "not read");
+// console.log(hobbit.info());
+
+function newBook() {
+    let answer = new Book(prompt("title"), prompt("author"), prompt("pages"), prompt("yes/no"));
+    addBookToLibrary(answer);     
+}
+
+console.log(library);
+console.log(answer.info());
