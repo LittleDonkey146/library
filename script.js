5b8cd78144be8c19a5ba06019f35d3ce9cddc47b
littledonkey146 <alexandrosgka@gmail.com>
2022-06-23 19:36:16 +0300

    Remove button on the works

--- script.js
+++ script.js
@@ -5,27 +5,35 @@ const addBook = document.querySelector(".add-book");
 const cardBox = document.querySelector(".cards-box");
 const popupBtn = document.querySelector(".popup");
 const submitBtn = document.querySelector(".submit");
+const form = document.getElementById("form");
+const removeBtn = document.querySelector(".remove-button");
+
+let bookTitle = ' ';
+let bookAuthor = ' ';
+let bookPages = ' ';
 
 addBook.addEventListener('click', newBook);
 submitBtn.addEventListener('click', closePopup);
+removeBtn.addEventListener('click', removeFunc);
+form.addEventListener('click', function(event) {
+    event.preventDefault();
+});
 
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function () {
-        return `${title} by ${author}, ${pages} pages, ${read}`;
-    }
 }
 
 function addBookToLibrary(newBook) {
     library.push(newBook);
+    console.log(newBook);
+    console.log('test');
 }
 
 function newBook() {
     popupBtn.style.display = "block";
-    ;
 }
 
 function readLibrary() {
@@ -34,19 +42,44 @@ function readLibrary() {
     }
 }
 
-function addBookToPage(book) {
+const title = form.elements['title'];
+const author = form.elements['author'];
+const pages = form.elements['pages'];
+
+function closePopup() {
+    
+    bookTitle = title.value;
+    bookAuthor = author.value;
+    bookPages = pages.value;
+
+    console.log(bookTitle);
+    console.log(bookAuthor);
+    console.log(bookPages);
+    popupBtn.style.display = "none";
+    addBookToLibrary(bookTitle);
+    addBookToPage();
+}
+
+function addBookToPage() {
     const newBook = document.createElement('div');
     const newBookElement = document.createElement('p');
     newBook.classList.add('cards');
-    newBookElement.textContent = `Title: ${book.title}` 
-    + ` Author: ${book.author}` 
-    + ` Pages: ${book.pages}` 
-    + ` Did you read it? ${book.read}`;
+
+    // Remove button creation
+    const newRemoveBtn = document.createElement('button')
+    newRemoveBtn.classList.add('remove-button');
+    newRemoveBtn.textContent = 'Remove';
+
+    newBookElement.textContent = `Title: ${bookTitle}` 
+    + ` Author: ${bookAuthor}` 
+    + ` Pages: ${bookPages}` 
+    + ` Did you read it?`; //INSERT READ OPTION HERE
     cardBox.appendChild(newBook);
     newBook.appendChild(newBookElement);
+    newBook.appendChild(newRemoveBtn);
 }
 
-function closePopup() {
-   
-    popupBtn.style.display = "none";
+function removeFunc() {
+    const removeThis = document.querySelector('.cards');
+    removeThis.parentNode.removeChild(removeThis);
 }
